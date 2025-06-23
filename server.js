43bf9b9d2155e8a62de45f509de35495e9a47929
Alexander Rodriguez <alexander314@gmail.com>
2025-06-23 15:01:01 -0500

    Primera versión del backend

---
+++ server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth');
+const path = require('path');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Conexión a MongoDB establecida'))
+.catch(err => console.error('Error conectando a MongoDB:', err));
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../Frontend/public')));
+
+app.use('/api', authRoutes);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Frontend/views/index.html'));
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
